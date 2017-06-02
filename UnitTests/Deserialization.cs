fe48105c05e94da51965b7f3e53e4a44bb4e421b
Maurice Prather <mprather@users.noreply.github.com>
2017-06-02 12:04:06 -0700

    Added unit tests to validate reading and writing

---
+++ UnitTests/Deserialization.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FSH;
+
+namespace UnitTests {
+	
+  [TestClass]
+	public class Deserialization {
+		
+	  [TestMethod]
+		public void GeneralStructure() {
+
+			ArchiveFile archive = new ArchiveFile();
+
+			using (FileStream stream = new FileStream(@"data\\archive - Sept 2016.fsh", FileMode.Open)) {
+				using (BinaryReader reader = new BinaryReader(stream, Encoding.ASCII)) {
+					archive.Deserialize(reader);
+				}
+			}
+
+			Assert.IsTrue(archive.Flobs.Count == 128, "Unexpected flob count encountered");
+      Assert.IsTrue(archive.Flobs.Any(f => f.Blocks.Any(b => b.Status == 0x00)), "Archive data was not located");
+			Assert.IsTrue(archive.Flobs[0].FlobType == FlobType.Data);
+			Assert.IsTrue(archive.Flobs[1].FlobType == FlobType.Last);
+			Assert.IsTrue(archive.Flobs[2].FlobType == FlobType.Empty);
+
+		}
+
+	}
+
+}
---
+++ UnitTests/Serialization.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FSH;
+
+namespace UnitTests {
+	
+  [TestClass]
+	public class Serialization {
+		
+	  [TestMethod]
+		public void BasicExportTest()  {
+			
+		  ArchiveFile archive = new ArchiveFile();
+
+			using (FileStream originalStream = new FileStream(@"data\\archive - Sept 2016.fsh", FileMode.Open)) 
+			using (BinaryReader originalReader = new BinaryReader(originalStream, Encoding.ASCII)) {
+
+        // Load the archive file...
+        archive.Deserialize(originalReader);
+
+        // Now let's export the file (mostly) untouched...
+        // ----------------------------------------------------------------------
+        // Note: TrackMetadata.Name is null padded. This is a difference.
+        // ----------------------------------------------------------------------
+        using (FileStream exportStream = new FileStream("exported.fsh", FileMode.Create))
+				using (BinaryWriter writer = new BinaryWriter(exportStream, Encoding.ASCII)) {
+					archive.Serialize(writer);
+			  }
+
+			  // Let's compare the two files...
+        using (FileStream exportedStream = new FileStream(@"exported.fsh", FileMode.Open))
+        using (BinaryReader exportedReader = new BinaryReader(exportedStream, Encoding.ASCII)) {
+
+          Assert.IsTrue(originalStream.Length == exportedStream.Length, "File size does not match");
+          
+          // Rewind the original stream...
+          originalStream.Seek(0, SeekOrigin.Begin);
+
+          byte originalByte;
+          byte exportedByte;
+
+          while (originalReader.PeekChar() != -1 && exportedReader.PeekChar() != -1) {
+
+            originalByte = originalReader.ReadByte();
+            exportedByte = exportedReader.ReadByte();
+
+            // ----------------------------------------------------------------------
+            // Note: TrackMetadata.Name is null padded but the original file has 
+            //       garbage chars. The nulls should be the only differences 
+            //       found in the exported file.
+            // ----------------------------------------------------------------------
+            switch (originalStream.Position) {
+              case 79433:
+                originalStream.Seek(2, SeekOrigin.Current);
+                exportedStream.Seek(2, SeekOrigin.Current);
+                break;
+              case 87223:
+                break;
+              case 91434:
+                originalStream.Seek(7, SeekOrigin.Current);
+                exportedStream.Seek(7, SeekOrigin.Current);
+                break;
+              default:
+                Assert.IsTrue(originalByte == exportedByte, "Difference located at " + originalStream.Position + ". " + originalByte.ToString("X") + " vs. " + exportedByte.ToString("X"));
+                break;
+            }
+
+          }
+
+        }  // End of using exportedStream
+
+      }  // End of using originalStream
+
+    }  // End of BasicExportTest
+
+  }  // End of Serialization class
+
+}  // End of the UnitTests namespace
