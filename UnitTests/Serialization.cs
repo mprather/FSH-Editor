550848ea9b2eb88ec9ac3ca11b741c3e7eacb502
Maurice Prather <mprather@users.noreply.github.com>
2017-06-02 15:58:06 -0700

    Switched to read into a buffer to speed up test.

--- UnitTests/Serialization.cs
+++ UnitTests/Serialization.cs
@@ -7,17 +7,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FSH;
 
 namespace UnitTests {
-	
-  [TestClass]
-	public class Serialization {
-		
-	  [TestMethod]
-		public void BasicExportTest()  {
-			
-		  ArchiveFile archive = new ArchiveFile();
 
-			using (FileStream originalStream = new FileStream(@"data\\archive - Sept 2016.fsh", FileMode.Open)) 
-			using (BinaryReader originalReader = new BinaryReader(originalStream, Encoding.ASCII)) {
+  [TestClass]
+  public class Serialization {
+
+    private static int BufferLength                  = 4096;
+
+    [TestMethod]
+    public void BasicExportTest() {
+
+      ArchiveFile archive = new ArchiveFile();
+
+      using (FileStream originalStream = new FileStream(@"data\\archive - Sept 2016.fsh", FileMode.Open))
+      using (BinaryReader originalReader = new BinaryReader(originalStream, Encoding.ASCII)) {
 
         // Load the archive file...
         archive.Deserialize(originalReader);
@@ -27,47 +29,61 @@ namespace UnitTests {
         // Note: TrackMetadata.Name is null padded. This is a difference.
         // ----------------------------------------------------------------------
         using (FileStream exportStream = new FileStream("exported.fsh", FileMode.Create))
-				using (BinaryWriter writer = new BinaryWriter(exportStream, Encoding.ASCII)) {
-					archive.Serialize(writer);
-			  }
+        using (BinaryWriter writer = new BinaryWriter(exportStream, Encoding.ASCII)) {
+          archive.Serialize(writer);
+        }
 
-			  // Let's compare the two files...
+        // Let's compare the two files...
         using (FileStream exportedStream = new FileStream(@"exported.fsh", FileMode.Open))
         using (BinaryReader exportedReader = new BinaryReader(exportedStream, Encoding.ASCII)) {
 
           Assert.IsTrue(originalStream.Length == exportedStream.Length, "File size does not match");
-          
+
           // Rewind the original stream...
           originalStream.Seek(0, SeekOrigin.Begin);
 
-          byte originalByte;
-          byte exportedByte;
+          byte[] originalBuffer = new byte[BufferLength];
+          byte[] exportedBuffer = new byte[BufferLength];
+
+          long position = 0;
 
           while (originalReader.PeekChar() != -1 && exportedReader.PeekChar() != -1) {
-
-            originalByte = originalReader.ReadByte();
-            exportedByte = exportedReader.ReadByte();
-
+            
             // ----------------------------------------------------------------------
-            // Note: TrackMetadata.Name is null padded but the original file has 
-            //       garbage chars. The nulls should be the only differences 
-            //       found in the exported file.
+            // Note: this method doesn't work well if the errors are within the boundary
+            //       of the buffer length. it works ok for what is being tested though.
             // ----------------------------------------------------------------------
-            switch (originalStream.Position) {
-              case 79433:
-                originalStream.Seek(2, SeekOrigin.Current);
-                exportedStream.Seek(2, SeekOrigin.Current);
-                break;
-              case 87223:
-                break;
-              case 91434:
-                originalStream.Seek(7, SeekOrigin.Current);
-                exportedStream.Seek(7, SeekOrigin.Current);
-                break;
-              default:
-                Assert.IsTrue(originalByte == exportedByte, "Difference located at " + originalStream.Position + ". " + originalByte.ToString("X") + " vs. " + exportedByte.ToString("X"));
-                break;
-            }
+
+            originalBuffer = originalReader.ReadBytes(BufferLength);
+            exportedBuffer = exportedReader.ReadBytes(BufferLength);
+
+            // Let's walk through bytes...
+            for (int i = 0; i < originalBuffer.Length; i++) {
+
+              // ----------------------------------------------------------------------
+              // Note: TrackMetadata.Name is null padded but the original file has 
+              //       garbage chars. The nulls should be the only differences 
+              //       found in the exported file.
+              // ----------------------------------------------------------------------
+              switch (position++) {
+                case 79432:
+                  i += 2;
+                  position += 2;
+                  break;
+                case 87222:
+                  i++;
+                  position++;
+                  break;
+                case 91433:
+                  i += 7;
+                  position += 7;
+                  break;
+                default:
+                  Assert.IsTrue(originalBuffer[i] == exportedBuffer[i], "Difference located at " + position + ". " + originalBuffer[i].ToString("X") + " vs. " + exportedBuffer[i].ToString("X"));
+                  break;
+              }
+
+            }  // End of for i
 
           }
 
