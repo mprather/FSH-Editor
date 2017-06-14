7207c436b3a57344af017294ec00be6fd321c1e8
Maurice Prather <mprather@users.noreply.github.com>
2017-06-13 22:19:44 -0700

    Added utility class

---
+++ Editor/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FSH;
+
+namespace Editor {
+
+  public static class Utilities {
+
+    public static string TrimmedString(string value, bool isComment) {
+      int maxLength = isComment ? SerializableData.MaximumCommentLength : SerializableData.MaximumNameLength;
+      if (value.Length > maxLength) {
+        value = value.Substring(0, maxLength);
+      }
+      return value;
+    }  // End of TrimmedString
+
+  }
+}
