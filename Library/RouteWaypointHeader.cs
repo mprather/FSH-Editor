ea1c349570f1187c24a8b3be3a863370d7b26d52
Maurice Prather <mprather@users.noreply.github.com>
2017-07-18 08:11:07 -0700

    Class updated to allow deletion of waypoints. Rename of Waypoints. This commit is simply a historic reference as the class will be completely removed in upcoming commits.

--- Library/RouteWaypointHeader.cs
+++ Library/RouteWaypointHeader.cs
@@ -8,6 +8,7 @@ This software has been released under GPL v3.0 license.
 
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace FSH {
 
@@ -15,39 +16,53 @@ namespace FSH {
 
 		private short a;
 
-		private short waypointCount;
-
-		public List<WaypointReference> Waypoints { get; set; }
+		public List<WaypointReference> ReferencedWaypoints { get; set; }
 
 		public RouteWaypointHeader() {
-			this.Waypoints = new List<WaypointReference>();
+			this.ReferencedWaypoints = new List<WaypointReference>();
     }  // End of ctor
 
     public override ushort CalculateSize() {
       int length = 2 + 2;
-      this.Waypoints.ForEach(rw => length += rw.CalculateSize());
+      this.ReferencedWaypoints.ForEach(rw => length += rw.CalculateSize());
       return (ushort)length;
     }  // End of CalculateSize
 
+    public bool Delete(long id) {
+
+      bool itemDeleted = false;
+
+      var q = this.ReferencedWaypoints.Find(m => m.ID == id);
+      if (q != null) {
+        this.ReferencedWaypoints.Remove(q);
+        itemDeleted = true;
+      }
+
+      return itemDeleted;
+
+    }  // End of Delete
+
     public override void Deserialize(BinaryReader reader) {
 
-			this.waypointCount = reader.ReadInt16();
-			this.a = reader.ReadInt16();
+      short waypointCount = reader.ReadInt16();
+			
+      this.a = reader.ReadInt16();
+      System.Diagnostics.Debug.Assert(a == 0, "Expected A value of 0 not found");
 
-			for (int i = 0; i < this.waypointCount; i++) {
+			for (int i = 0; i < waypointCount; i++) {
 				WaypointReference wp = new WaypointReference(true);
 				wp.Deserialize(reader);
-				this.Waypoints.Add(wp);
+				this.ReferencedWaypoints.Add(wp);
 			}
 
     }  // End of Deserialize
 
     public override void Serialize(BinaryWriter writer) {
-			
-		  writer.Write(this.waypointCount);
+
+      writer.Write((short)this.ReferencedWaypoints.Count);
 			writer.Write(this.a);
 
-			this.Waypoints.ForEach(wp => wp.Serialize(writer));
+			this.ReferencedWaypoints.ForEach(wp => wp.Serialize(writer));
 
     }  // End of Serialize
 
