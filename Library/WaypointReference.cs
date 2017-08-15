3b772e34a2f555a39828cc5ab2825960592869de
Maurice Prather <mprather@users.noreply.github.com>
2017-08-15 10:00:00 -0700

    Comment update

--- Library/WaypointReference.cs
+++ Library/WaypointReference.cs
@@ -14,6 +14,13 @@ namespace FSH {
 	  
     private bool manageAllProperties;
 
+    /// <summary>
+    /// Unique ID. If the parent waypoint is a member of a Group, this value 
+    /// matches the id of the parent. This provides a relationship between 
+    /// Groups and WaypointReferences.
+    /// However, if the parent waypoint is a standalone waypoint, this value
+    /// appears to be unique and does not provide any relational assistance.
+    /// </summary>
     public long ID { get; set; }
 
     public override ushort CalculateSize() {
@@ -31,6 +38,7 @@ namespace FSH {
       }
 
       base.Deserialize(reader);
+      
     }  // End of Deserialize
 
     public override void Serialize(BinaryWriter writer) {
