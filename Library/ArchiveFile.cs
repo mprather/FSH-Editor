aa527902427ea32aa93b2b2a6263db0763a0fad3
Maurice Prather <mprather@users.noreply.github.com>
2017-08-15 18:19:42 -0700

    Added new static property Current. This allows the class to surface a method that can be used to check whether a waypoint is in use or not.
    Added UpdateActiveWaypoints

--- Library/ArchiveFile.cs
+++ Library/ArchiveFile.cs
@@ -9,6 +9,7 @@ This software has been released under GPL v3.0 license.
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace FSH {
 
@@ -25,10 +26,14 @@ namespace FSH {
 		private short d;
 		private short e;
 
+    private List<WaypointReference> activeWaypoints = null;
+
+    public static ArchiveFile Current { get; set; }
+
 		public List<Flob> Flobs { get; set; }
 
 		public ArchiveFile() {
-			
+      
 			this.header = new byte[16];
 			this.header = System.Text.Encoding.ASCII.GetBytes("RL90 FLASH FILE\0");
 			
@@ -39,10 +44,12 @@ namespace FSH {
 			this.d      = 1;
 			this.e      = 1;
 
-			//this.Headers = new List<FlobHeader>();
 			this.Flobs  = new List<Flob>();
+      this.activeWaypoints = new List<WaypointReference>();
 
-		}
+      ArchiveFile.Current = this;
+            
+		}  // End of ctor
 		
 		public override void Deserialize(BinaryReader reader) {
       
@@ -73,9 +80,26 @@ namespace FSH {
 
 			System.Diagnostics.Debug.Assert(this.flobCount == this.Flobs.Count);
 
-		}  // End of Deserialize
-		
-		public override void Serialize(BinaryWriter writer) {
+      UpdateActiveWaypoints();
+
+    }  // End of Deserialize
+
+    public void UpdateActiveWaypoints() {
+
+      this.activeWaypoints.Clear();
+
+      this.Flobs.ForEach(f => {
+        foreach (var q in f.Blocks.Where(b => b.Status != 0 && b.Type == BlockType.Route)) {
+          Route r = q.Data as Route;
+          foreach (var w in r.ReferencedWaypoints.Where(rw => !this.activeWaypoints.Any(a => a.ID == rw.ID))) {
+            this.activeWaypoints.Add(w);
+          }
+        }
+      });
+
+    }  // End of UpdateActiveWaypoints
+
+    public override void Serialize(BinaryWriter writer) {
 
 			this.flobCount = (short) this.Flobs.Count;
 
@@ -90,7 +114,14 @@ namespace FSH {
 			this.Flobs.ForEach(f => f.Serialize(writer));
 
 		}  // End of Serialize
-
+    
+    public bool WaypointInUse(long id, string name, double latitude, double longitude) {
+     return activeWaypoints.Any(a => a.ID == id ||
+                                         // The following is used for standalone waypoints since ID's won't match...
+                                         a.Data.Name == name && a.Data.Latitude == latitude && a.Data.Longitude == longitude
+                                        );
+    }  // End of WaypointInUse
+    
   }  // End of ArchiveFile class
 
 }
